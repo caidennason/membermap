5a5278963b0de23b346f982981b63d75e6c4fceb
caiden nason <caidenbnason@gmail.com>
2024-02-07 15:28:49 -0800

    map is working in react

--- usmembermap/src/App.js
+++ usmembermap/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import USAMap from 'react-usa-map';
 
 function App() {
+  const mapHandler = (event) => {
+    alert(event.target.dataset.name);
+  };
+
   return (
     <div className="App">
-
+      <USAMap onClick={mapHandler} />
     </div>
   );
 }
